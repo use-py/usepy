2691111fa7a1f10b5bbf54a943c7752b69d052d9
miclon <jcnd@163.com>
2023-02-23 10:54:29 +0800

    🧪 test: `useHtmlClean`用于清除网页标签

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -46,3 +46,10 @@ def test_header_to_dict():
 
 def test_unique_id():
     assert len(useUniqueId()) == 32
+
+
+def test_clean_html():
+    test_html = """<div class="test"><p>test</p></div>"""
+    assert useCleanHtml(test_html) == "test"
+    test_html2 = """<div class="test"><p>test</p><p>test2</p></div>"""
+    assert useCleanHtml(test_html2, white_tags=["p"]) == "<p>test</p><p>test2</p>"
