dabb1c8661704ae632e98fc9badf6dbfdee5cf39
NowAnti <believel.y@qq.com>
2023-02-24 10:25:02 +0800

    🧪 test: fix `demo_to`

--- example/use_to.py
+++ example/use_to.py
@@ -3,14 +3,14 @@ from usepy import useTo
 
 def demo_to():
     print("demo_to")
-    print(useTo.to_string('https://www.baidu.com'))
-    print(useTo.to_string(b'https://www.baidu.com'))
-    print(useTo.to_string(123))
-    print(useTo.to_string(123.456))
-    print(useTo.to_string(True))
-    print(useTo.to_string(None))
-    print(useTo.to_string([1, 2, 3]))
-    print(useTo.to_string({'a': 1, 'b': 2, 'c': 3}))
+    print(useTo.string('https://www.baidu.com'))
+    print(useTo.string(b'https://www.baidu.com'))
+    print(useTo.string(123))
+    print(useTo.string(123.456))
+    print(useTo.string(True))
+    print(useTo.string(None))
+    print(useTo.string([1, 2, 3]))
+    print(useTo.string({'a': 1, 'b': 2, 'c': 3}))
     pass
 
 
