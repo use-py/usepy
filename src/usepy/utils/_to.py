9cab555fe39d69b8e8e729b8a63e3e9d2948fd66
miclon <jcnd@163.com>
2023-02-26 14:08:19 +0800

    ✨ feat(useTo): `camel` `snake`

--- src/usepy/utils/_to.py
+++ src/usepy/utils/_to.py
@@ -4,6 +4,7 @@
     @Description: 用于转换数据类型的工具函数
 """
 import hashlib
+import re
 from typing import Any, AnyStr
 
 
@@ -36,3 +37,35 @@ def sha1(data: AnyStr) -> str:
     :return: sha1
     """
     return hashlib.sha1(string(data).encode()).hexdigest()
+
+
+def camel(data: str, char: str = '-') -> str:
+    """
+    将字符数据转换为驼峰命名
+    :param data: data
+    :param char: 特征字符，如：-、_
+    :return:
+    >>> camel("test")
+    'test'
+    >>> camel("test-case")
+    'testCase'
+    >>> camel("test_case", char="_")
+    'testCase'
+    """
+    return re.sub(char + r'(\w)', lambda m: m.group(1).upper(), data)
+
+
+def snake(data: str, char: str = '_') -> str:
+    """
+    将字符数据转换为下划线命名
+    :param data: data
+    :param char: 特征字符，如：-、_
+    :return:
+    >>> snake("test")
+    'test'
+    >>> snake("testCase")
+    'test_case'
+    >>> snake("testCase", char="-")
+    'test-case'
+    """
+    return re.sub(r'([A-Z])', lambda m: char + m.group(1).lower(), data)
