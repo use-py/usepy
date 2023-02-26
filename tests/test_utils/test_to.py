7ef1231bcd2473fca75ba7f2e53a8eca0bab322b
miclon <jcnd@163.com>
2023-02-26 14:08:37 +0800

    🧪 test(useTo): `camel` `snake`

--- tests/test_utils/test_to.py
+++ tests/test_utils/test_to.py
@@ -10,3 +10,15 @@ def test_to_string():
     assert useTo.string(None) == "None"
     assert useTo.string([1, 2, 3]) == "[1, 2, 3]"
     assert useTo.string({"code": 1000}) == "{'code': 1000}"
+
+
+def test_to_camel():
+    assert useTo.camel("test") == "test"
+    assert useTo.camel("test-case") == "testCase"
+    assert useTo.camel("test_case", char="_") == "testCase"
+
+
+def test_to_snake():
+    assert useTo.snake("test") == "test"
+    assert useTo.snake("testCase") == "test_case"
+    assert useTo.snake("testCase", char="-") == "test-case"
