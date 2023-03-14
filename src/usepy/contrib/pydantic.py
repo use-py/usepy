6276c72c9e38b55e2902988c7626f3a35c60954a
miclon <jcnd@163.com>
2023-03-14 16:37:33 +0800

    ✨ feat: contrib for `pydantic`

---
+++ src/usepy/contrib/pydantic.py
@@ -0,0 +1,67 @@
+from jmespath import search
+from pydantic import BaseModel, Field
+from pydantic.utils import GetterDict
+
+
+class Getter(GetterDict):
+    """
+    自定义 GetterDict，用于 pydantic 的 orm_mode
+    """
+
+    def get(self, key, default):  # noqa
+        model, data = self._obj['model'], self._obj['data']
+        for name, field in model.__fields__.items():
+            path = field.field_info.extra.get('path')
+            if path and name == key:
+                return search(path, data)
+        return default
+
+
+class BaseORMModel(BaseModel):
+    """
+    自定义 BaseModel，用于 pydantic 的 orm_mode
+
+    example:
+    class Project(BaseORMModel):
+        id: str = Field(..., path="detail.id")
+        url: str = Field(..., path="detail.url")
+        author_name: str = Field(..., path="detail.authors[*].name")
+
+    test_data = {
+        "detail": {
+            "id": "123",
+            "url": "https://www.baidu.com",
+            "authors": [
+                {"name": "张三"},
+                {"name": "李四"},
+            ]
+        }
+    }
+    project = Project.from_orm({"model": Project, "data": test_data}).dict()
+    {'id': '123', 'url': 'https://www.baidu.com', 'author_name': ['张三', '李四']}
+    """
+
+    class Config:
+        orm_mode = True
+        getter_dict = Getter
+
+
+if __name__ == '__main__':
+    class Project(BaseORMModel):
+        id: str = Field(..., path="detail.id")
+        url: str = Field(..., path="detail.url")
+        author_name: list = Field(..., path="detail.authors[*].name")
+
+
+    test_data = {
+        "detail": {
+            "id": "123",
+            "url": "https://www.baidu.com",
+            "authors": [
+                {"name": "张三"},
+                {"name": "李四"},
+            ]
+        }
+    }
+    project = Project.from_orm({"model": Project, "data": test_data}).dict()
+    print(project)
